632413458f90683da4f67cb761452d2119112b89
yangyihong <2264710047@qq.com>
2018-10-17 11:53:09 +0800

    first commit blog

---
+++ blog/views.py
@@ -0,0 +1,18 @@
+'''
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+
+def index(request):
+	posts = Post.objects.order_by('-created_date')
+	return render(request,'blog/post_list.html',{'posts':posts})
+'''
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
+def index(request):
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def show(request,post_id):
+    post = get_object_or_404(Post,pk=post_id)
+    return render(request,'blog/post_detail.html',{'post':post})
